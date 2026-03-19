278d1d52710fb4c46b5452dcdd27dc2a5b57b62e
rroslin <raymark.roslin@gmail.com>
2026-03-19 08:03:50 +0800

    feat: add page break when printing

--- src/index.tsx
+++ src/index.tsx
@@ -9,43 +9,59 @@ import AwardItem from './components/AwardItem'
 import './index.css'
 
 function Application() {
-  const resume = fixture as Resume;
-  document.title = `${resume.profile.firstName} ${resume.profile.lastName.charAt(0)}.`;
+	const resume = fixture as Resume;
+	document.title = `${resume.profile.firstName} ${resume.profile.lastName.charAt(0)}.`;
 
-  return (
-    <main>
-		<ProfileSection profile={resume.profile} />
-		<section>
-			<span>{resume.summary}</span>
-		</section>
-		<section>
-			<h2>Experience</h2>
-			<For each={resume.experiences}>
-			{(experience) => <ExperienceArticle experience={experience} />}
-			</For>
-		</section>
-		<section>
-			<h2>Skills</h2>
-			<div class="skills">
-				<For each={resume.skills}>
-				{(skill) => <div class="skills__badge">{skill}</div>}
-				</For>
-			</div>
-		</section>
-		<section>
-			<h2>Education</h2>
-			<For each={resume.educations}>
-			{(education) => <EducationItem education={education}/>}
-			</For>
-		</section>
-		<section>
-			<h2>Awards</h2>
-			<For each={resume.awards}>
-			{(award) => <AwardItem award={award}/>}
-			</For>
-		</section>
-    </main>
-  )
+	return (
+		<main>
+			<article class="page">
+				<ProfileSection profile={resume.profile} />
+				<section class="page__section --avoid-break-inside">
+					<span>{resume.summary}</span>
+				</section>
+				<section class="page__section --avoid-break-inside">
+					<h2>Experience</h2>
+					<For each={resume.experiences}>
+						{(experience) => (
+							<div class="page__block --avoid-break-inside">
+								<ExperienceArticle experience={experience} />
+							</div>
+						)}
+					</For>
+				</section>
+			</article>
+			<article class="page">
+				<section class="page__section --avoid-break-inside">
+					<h2>Skills</h2>
+					<div class="skills">
+						<For each={resume.skills}>
+							{(skill) => <div class="skills__badge">{skill}</div>}
+						</For>
+					</div>
+				</section>
+				<section class="page__section --avoid-break-inside">
+					<h2>Education</h2>
+					<For each={resume.educations}>
+						{(education) => (
+							<div class="page__block --avoid-break-inside">
+								<EducationItem education={education} />
+							</div>
+						)}
+					</For>
+				</section>
+				<section class="page__section --avoid-break-inside">
+					<h2>Awards</h2>
+					<For each={resume.awards}>
+						{(award) => (
+							<div class="page__block --avoid-break-inside">
+								<AwardItem award={award} />
+							</div>
+						)}
+					</For>
+				</section>
+			</article>
+		</main>
+	)
 }
 
 const root = document.getElementById('root')
