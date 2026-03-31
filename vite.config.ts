b122ac9afc87c78f08129fae78a878a0a4a8209b
rroslin <raymark.roslin@gmail.com>
2026-03-31 09:17:13 +0800

    refactor: add path aliasing

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
 
 export default defineConfig({
-  plugins: [solid()],
+	plugins: [solid()],
+	resolve: {
+		alias: {
+			'~': path.resolve(__dirname, './src/')
+		}
+	}
 })
