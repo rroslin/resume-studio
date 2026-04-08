5b7233e487db23684c0dcf8845b0e0bd0ebfd123
rroslin <raymark.roslin@gmail.com>
2026-04-08 11:49:33 +0800

    feat: implement print button

--- src/pages/preview/PreviewPage.tsx
+++ src/pages/preview/PreviewPage.tsx
@@ -7,41 +7,49 @@ import EducationItem from './EducationItem'
 import AwardItem from './AwardItem'
 
 import "./PreviewPage.css";
+import Icon from '~/components/Icon'
 
 function PreviewPage() {
 	const { resume } = useResumeContext();
-
 	return (
-		<div class="preview-page">
-			<section>
-				<ProfileArticle {...resume.profile} />
-			</section>
-			<section class='--allow-break'>
-				<h2>Experience</h2>
-				<For each={[...resume.experiences].reverse()}>
-					{(experience) => <ExperienceArticle {...experience} />}
-				</For>
-			</section>
-			<section>
-				<h2>Skills</h2>
-				<div class="skills">
-					<For each={resume.skills}>
-						{(skill) => <div class="badge">{skill}</div>}
+		<div class="preview-view">
+			<div class="preview-toolbar">
+				<button class="print-button" type="button" onClick={() => window.print()}>
+					<Icon name="printer"/>
+				</button>
+			</div>
+
+			<div class="preview-page">
+				<section>
+					<ProfileArticle {...resume.profile} />
+				</section>
+				<section class='--allow-break'>
+					<h2>Experience</h2>
+					<For each={[...resume.experiences].reverse()}>
+						{(experience) => <ExperienceArticle {...experience} />}
 					</For>
-				</div>
-			</section>
-			<section>
-				<h2>Education</h2>
-				<For each={[...resume.educations].reverse()}>
-					{(education) => <EducationItem {...education} />}
-				</For>
-			</section>
-			<section>
-				<h2>Awards</h2>
-				<For each={[...resume.awards].reverse()}>
-					{(award) => <AwardItem {...award} />}
-				</For>
-			</section>
+				</section>
+				<section>
+					<h2>Skills</h2>
+					<div class="skills">
+						<For each={resume.skills}>
+							{(skill) => <div class="badge">{skill}</div>}
+						</For>
+					</div>
+				</section>
+				<section>
+					<h2>Education</h2>
+					<For each={[...resume.educations].reverse()}>
+						{(education) => <EducationItem {...education} />}
+					</For>
+				</section>
+				<section>
+					<h2>Awards</h2>
+					<For each={[...resume.awards].reverse()}>
+						{(award) => <AwardItem {...award} />}
+					</For>
+				</section>
+			</div>
 		</div>
 	)
 }
